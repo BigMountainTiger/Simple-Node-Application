875da8911b26d6fd8102c5059ebf52eebb2c1a8d
Song Li <da_tou_li@yahoo.com>
2015-11-18 15:05:52 -0500

    Added additional method in the controller

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var app = express();
 
 var routes = require('./routes')
 app.get('/', routes.index);
+app.get('/json', routes.getJson);
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
-exports.index = function(req, res){
-  res.json({ a: 1 });
+exports.index = function(req, res) {
+	res.json({ a: 1 });
+};
+
+exports.getJson = function(req, res) {
+	console.log("This is called");
+	res.json({ a: 1, b: 2 });
 };
